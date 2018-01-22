7ba0b9d48aad2d35f5e1ac92b2892648aa6c76e5
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2018-01-22 11:08:49 +0000

    Deleted files

--- Calculatortest/src/main/scala/Calculator/Simplecalc.scala
+++ Calculatortest/src/main/scala/Calculator/Simplecalc.scala
@@ -1,43 +1,44 @@
 package Calculator
 
-  import scala.math._
+import scala.language.postfixOps
+import scala.math._
 
   class Simplecalc {
 
 
     val button0: Int = 0
-    val button1 = (1)
-    val button2 = (2)
-    val button3 = (3)
-    val button4 = (4)
-    val button5 = (5)
-    val button6 = (6)
-    val button7 = (7)
-    val button8 = (8)
-    val button9 = (9)
+    val button1 = 1
+    val button2 = 2
+    val button3 = 3
+    val button4 = 4
+    val button5 = 5
+    val button6 = 6
+    val button7 = 7
+    val button8 = 8
+    val button9 = 9
 
     def add(firstButton: Int, secondButton: Int): Int = {
        firstButton + secondButton
     }
 
-    def subtract(button3: Int, button2: Int): Int = {
-      3 - 2
+    def subtract(firstButton: Int, secondButton: Int): Int = {
+      firstButton - secondButton
     }
 
-    def devide(button4: Int, Button2: Int): Int = {
-      4 / 2
+    def devide(firstButton: Int, secondButton: Int): Int = {
+      firstButton / secondButton
     }
 
-    def multiply(button3: Int, Button2: Int): Int = {
-      3 * 2
+    def multiply(firstButton: Int, secondButton: Int): Int = {
+      firstButton * secondButton
     }
 
     def power(value: Int, toPowerOf: Int): Int = {
       pow(value, toPowerOf).toInt
     }
 
-      "butto1 add button2" should return { Int 3
-    }
-
   }
 
+//divided by its self. is the number a whole number no. show error, if true show int
+//took a number split in to square factors.
+//squared
