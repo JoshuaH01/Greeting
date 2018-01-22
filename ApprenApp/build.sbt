70fe7ddd26138f28ee51a6e2b749bd0364157054
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2018-01-22 11:07:52 +0000

    |WIP| appren scala practise

---
+++ ApprenApp/build.sbt
@@ -0,0 +1,5 @@
+name := "ApprenApp"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
---
+++ ApprenApp/src/main/scala/App/Greetings.scala
@@ -0,0 +1,10 @@
+package App
+
+object Greetings extends App {
+
+  val name = "Joshua"
+
+ def greet(name: String) : Unit = println("Hello" + name + "!")
+greet("Joshua")
+
+}
