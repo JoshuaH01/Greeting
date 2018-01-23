3625cd4c0b99571c1ac7c071820712d4f8abfb93
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2018-01-23 12:00:09 +0000

    |WIP| refactored code, added additinal def speak

--- ApprenApp/src/main/scala/App/Greetings.scala
+++ ApprenApp/src/main/scala/App/Greetings.scala
@@ -1,21 +1,43 @@
 package App
 
+import App.Greetings.name
+
 import scala.io.StdIn
+
+class Person(name:String, age:Int) {
+
+  def speak() : String = {
+    if(name =="Joshua") {
+      "You don't get a hello!!!"
+    } else {
+      "hello " + name
+    }
+  }
+
+  object Prompt {
+
+    def ask(message: String) : String = StdIn.readLine(message)
+
+  }
+}
 
 object Greetings extends App {
 
-  def greet(name: String): Unit = {
+ /* def greet(name: String): Unit = {
     if (name == "Joshua") {
       println("You don't get a hello.")
     } else {
       println("Hello " + name)
 
     }
-  }
+  }*/
 
-    val name = StdIn.readLine("What is your name? ")
+    val name : String = Prompt.ask("What is your name? ")
 
-    greet(name)
+    val p = new Person(name)
+
+    println(p.speak())
+
 
   }
 
