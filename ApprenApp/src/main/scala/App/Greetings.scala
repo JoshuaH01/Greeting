7951588b77668a7836995d4bf2162ce0537bcecf
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2018-01-24 07:53:14 +0000

    Changed app to bank account, added cashisa and savings. Also ran it sucsessfuly

--- ApprenApp/src/main/scala/App/Greetings.scala
+++ ApprenApp/src/main/scala/App/Greetings.scala
@@ -1,8 +1,26 @@
 package App
 
-import App.Greetings.name
-
 import scala.io.StdIn
+
+abstract class BankAccount(accountNumber : String,balance: Double) {
+  def withdraw(amount: Double) : BankAccount
+  def deposit(amount: Double) : BankAccount
+}
+
+class SavingsAccount(accountNumber: String, balance: Double) extends  BankAccount(accountNumber, balance){
+
+  override def deposit(amount: Double): BankAccount = new SavingsAccount(accountNumber, balance + amount)
+  override def withdraw(amount: Double): BankAccount = new SavingsAccount(accountNumber, balance - amount)
+
+}
+
+final class CashISASavingsAccount (accountNumber: String, balance: Double) extends BankAccount(accountNumber, balance) {
+  override def deposit(amount: Double): BankAccount = new SavingsAccount(accountNumber, balance + amount)
+  override def withdraw(amount: Double): BankAccount = {
+    println(s"You can't withdraw yet, your money is locked in for 3 years!")
+    this
+  }
+}
 
 class Person(name:String, age:Int) {
 
@@ -13,24 +31,29 @@ class Person(name:String, age:Int) {
       "hello " + name + ",you are " + age + " years old"
     }
   }
+  private val years	: String = if (age == 1) "years" else "year"
  }
+
+object Prompt {
+
+  def ask(message: String) : String = StdIn.readLine(message)
+
+}
 
 object Greetings extends App {
 
 
-  object Prompt {
-
-    def ask(message: String) : String = StdIn.readLine(message)
-
-  }
 
 
-    val name : String = Prompt.ask("What is your name? ")
-  val age = Prompt.ask("How old are you?")
+//    val n : String = Prompt.ask("What is your name? ")
+//  val a = Prompt.ask("How old are you?")
+//
+//    val p = new Person(n,a.toInt)
+//  println(p.speak())
 
-    val p = new Person(name,age.toInt)
-  println(p.speak())
-
+  val accountNumber = "12345"
+  val account = new CashISASavingsAccount(accountNumber, 100.00)
+  val afterWithdraw = account.withdraw(100.00)
 
   }
 
